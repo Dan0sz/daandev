370a323e3808465a28c066901b1498857d1e280c
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2025-04-05 22:32:29 +0200

    Added: Cart icon

--- src/Theme.php
+++ src/Theme.php
@@ -208,7 +208,7 @@ class Theme {
             <div class="daandev-menu ml-6 flex items-center gap-2 md:gap-4">
                 <ul id="daan-dev-menu" class="menu daan-dev-account">
                     <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item--has-toggle">
-                        <a href="<?php echo is_user_logged_in() ? '' : home_url( 'account' ); ?>" class="menu-link">
+                        <a href="<?php echo is_user_logged_in() ? '#' : home_url( 'account' ); ?>" class="menu-link">
                             <svg class="fill-current w-[1em] h-[1em] block text-xl" viewBox="0 0 448 512">
                                 <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512l388.6 0c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304l-91.4 0z"></path>
                             </svg>
@@ -228,6 +228,17 @@ class Theme {
 							);
 							?>
 						<?php endif; ?>
+                    </li>
+                    <li class="menu-item menu-item-type-custom menu-item-object-custom">
+                        <a class="relative" href="<?php echo edd_get_cart_contents() ? edd_get_checkout_uri() : '#'; ?>">
+                            <svg class="fill-current w-[1em] h-[1em] text-xl" viewBox="0 0 576 512">
+                                <path d="M0 24C0 10.7 10.7 0 24 0L69.5 0c22 0 41.5 12.8 50.6 32l411 0c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3l-288.5 0 5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5L488 336c13.3 0 24 10.7 24 24s-10.7 24-24 24l-288.3 0c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5L24 48C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"></path>
+                            </svg>
+                            <div class="<?php echo edd_get_cart_contents() ? '' :
+								'hidden'; ?> absolute rounded-full text-white bg-primary-500 w-3.5 h-3.5 flex items-center justify-center text-xs right-0 top-1">
+                                <span class="cart-count"><?php echo edd_get_cart_quantity(); ?></span>
+                            </div>
+                        </a>
                     </li>
                 </ul>
             </div>
