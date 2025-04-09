4d62760acbc3d49dab78e42322826c84ca8e4ac4
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2025-04-09 15:00:53 +0200

    Separated source and distro assets, to create a cleaner release package.

--- src/Theme.php
+++ src/Theme.php
@@ -94,17 +94,17 @@ class Theme {
 	 * Enqueue styles and Tailwind CSS
 	 */
 	public function daandev_enqueue_scripts() {
-		$file_modified = filemtime( get_stylesheet_directory() . '/assets/js/edd-add-to-cart.min.js' );
+		$file_modified = filemtime( get_stylesheet_directory() . '/assets/dist/js/edd-add-to-cart.min.js' );
 
-		wp_enqueue_script( 'daan-dev-add-to-cart', get_stylesheet_directory_uri() . '/assets/js/edd-add-to-cart.min.js', [], $file_modified, [ 'defer' => true ] );
+		wp_enqueue_script( 'daan-dev-add-to-cart', get_stylesheet_directory_uri() . '/assets/dist/js/edd-add-to-cart.min.js', [], $file_modified, [ 'defer' => true ] );
 
 		// Parent theme styles
 		wp_enqueue_style( 'kadence-theme', get_template_directory_uri() . '/style.css' );
 
-		$file_modified = filemtime( get_stylesheet_directory() . '/assets/css/output.css' );
+		$file_modified = filemtime( get_stylesheet_directory() . '/assets/dist/css/output.css' );
 
 		// Tailwind CSS compiled file
-		wp_enqueue_style( 'daan-dev-tailwind', get_stylesheet_directory_uri() . '/assets/css/output.css', [ 'kadence-theme' ], $file_modified );
+		wp_enqueue_style( 'daan-dev-tailwind', get_stylesheet_directory_uri() . '/assets/dist/css/output.css', [ 'kadence-theme' ], $file_modified );
 	}
 
 	/**
