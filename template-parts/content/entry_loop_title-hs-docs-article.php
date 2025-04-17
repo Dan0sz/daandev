c02048b6042a45b51dcff9f76e42dbb562d96ac4
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2025-04-17 10:06:24 +0200

    Fixed: Output loop entries in search just like any other docs archive and don't display category titles in search.

--- template-parts/content/entry_loop_title-hs-docs-article.php
+++ template-parts/content/entry_loop_title-hs-docs-article.php
@@ -15,9 +15,9 @@ $title_element = kadence()->option(
 	]
 );
 if ( isset( $title_element ) && is_array( $title_element ) && true === $title_element[ 'enabled' ] ) {
-	if ( ( ! is_post_type_archive( 'hs-docs-article' ) && ! is_tax( 'hs-docs-category' ) ) && ( is_search() || is_archive() || is_home() ) ) {
+	if ( ( ! is_post_type_archive( 'hs-docs-article' ) && ! is_tax( 'hs-docs-category' ) ) && ( ! is_search() && ( is_archive() || is_home() ) ) ) {
 		the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-	} elseif ( is_post_type_archive( 'hs-docs-article' ) || is_tax( 'hs-docs-category' ) ) {
+	} elseif ( is_search() || is_post_type_archive( 'hs-docs-article' ) || is_tax( 'hs-docs-category' ) ) {
 		the_title(
 			'<div class="entry-title md:flex md:gap-3 lg:gap-4 items-center md:mb-4 text-md font-brand"><a class="!text-neutral-900" href="' .
 			esc_url( get_permalink() ) .
